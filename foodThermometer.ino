5ad33a3759b97a467a010b297b3b603046cf2490
Dominique Tardif <dommtardif@gmail.com>
2018-05-13 17:31:04 -0400

    refactored to PROGMEM

--- foodThermometer.ino
+++ foodThermometer.ino
@@ -11,20 +11,16 @@
 #define SMLINE_HEIGHT 8
 #define LINE_HEIGHT 11
 
-#define SOFTTHREAD_MIN 10500
-#define THREAD_MIN 10700
-#define SOFTBALL_MIN 11200
-#define FIRMBALL_MIN 11800
-#define HARDBALL_MIN 12500
-#define SOFTCRACK_MIN 13500
-#define HARDCRACK_MIN 14500
-#define CARAMEL_MIN 15000
-#define BURNTSUGAR_MIN 17100
-
 #define DEFAULT_MODE 1
 #define CANDY_MODE 2
-#define MEAT_MODE 3
-#define ALARM_MODE 4
+#define BEEF_MODE 3
+#define PORC_MODE 4
+#define POULTRY_MODE 5
+#define ALARM_MODE 6
+
+typedef struct {
+  char description [25];
+} descriptionType;
 
 U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 Adafruit_MAX31865 max3 = Adafruit_MAX31865(13, 12, 15, 2);
@@ -34,7 +30,7 @@ byte tmode = DEFAULT_MODE;
 int alarmTemp = 0;
 
 void setup(void) {
-  Serial.begin(115200);
+  //Serial.begin(115200);
   pinMode(0, INPUT);
   pinMode(14, INPUT_PULLUP);
   pinMode(16, INPUT_PULLUP);
@@ -47,75 +43,61 @@ void setup(void) {
 boolean checkForFaults() {
   uint8_t fault = max3.readFault();
   if (fault) {
+
+    u8g2.firstPage();
+    do {
+      u8g2.setFont(SMALL_FONT);
+      u8g2.setCursor(0, SMLINE_HEIGHT);
+      u8g2.print(F("Erreur"));
+      u8g2.setCursor(0, 2 * SMLINE_HEIGHT);
+
+      u8g2.print(F("Fault 0x"));
+      u8g2.print(fault, HEX);
+      u8g2.setCursor(0, 3 * SMLINE_HEIGHT);
+
+      if (fault & MAX31865_FAULT_HIGHTHRESH) {
+        u8g2.print(F("RTD High Threshold"));
+      }
+      if (fault & MAX31865_FAULT_LOWTHRESH) {
+        u8g2.print(F("RTD Low Threshold"));
+      }
+      if (fault & MAX31865_FAULT_REFINLOW) {
+        u8g2.print(F("REFIN- > 0.85 x Bias"));
+      }
+      if (fault & MAX31865_FAULT_REFINHIGH) {
+        u8g2.print(F("REFIN- < 0.85 x Bias - FORCE- open"));
+      }
+      if (fault & MAX31865_FAULT_RTDINLOW) {
+        u8g2.print(F("RTDIN- < 0.85 x Bias - FORCE- open"));
+      }
+      if (fault & MAX31865_FAULT_OVUV) {
+        u8g2.print(F("Under/Over voltage"));
+      }
+    } while ( u8g2.nextPage() );
     max3.clearFault();
+    delay(2000);
+
     return true;
   }
   return false;
 }
 
-void candyMaking(int currentTemp, int currentDelta, byte pos) {
-  char SOFTTHREAD[]  = "Petit filet/Soft thread";
-  char THREAD[]      = "Grand filet/Thread";
-  char SOFTBALL[]    = "Petit boule/Soft ball";
-  char FIRMBALL[]    = "Grand boule/Firm ball";
-  char HARDBALL[]    = "Boule/Hard ball";
-  char SOFTCRACK[]   = "Petit casse/Soft crack";
-  char HARDCRACK[]   = "Grand casse/Hard crack";
-  char CARAMEL[]     = "Caramel";
-  char BURNTSUGAR[]  = "Sucre brule/Burnt sugar";
-  unsigned int timeUntil;
-  const char* currentStage;
-  const char* nextStage;
-
-
-  if (currentTemp < SOFTTHREAD_MIN) {
-    currentStage = "";
-    timeUntil = ((currentDelta > 0) ? (int)((SOFTTHREAD_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = SOFTTHREAD;
-  } else if (currentTemp < THREAD_MIN) {
-    currentStage = SOFTTHREAD;
-    timeUntil = ((currentDelta > 0) ? (int)((THREAD_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = THREAD;
-  } else if (currentTemp < SOFTBALL_MIN) {
-    currentStage = THREAD;
-    timeUntil = ((currentDelta > 0) ? (int)((SOFTBALL_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = SOFTBALL;
-  } else if (currentTemp < FIRMBALL_MIN) {
-    currentStage = SOFTBALL;
-    timeUntil = ((currentDelta > 0) ? (int)((FIRMBALL_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = FIRMBALL;
-  } else if (currentTemp < HARDBALL_MIN) {
-    currentStage = FIRMBALL;
-    timeUntil = ((currentDelta > 0) ? (int)((HARDBALL_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = HARDBALL;
-  } else if (currentTemp < SOFTCRACK_MIN) {
-    currentStage = HARDBALL;
-    timeUntil = ((currentDelta > 0) ? (int)((SOFTCRACK_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = SOFTCRACK;
-  } else if (currentTemp < HARDCRACK_MIN) {
-    currentStage = SOFTCRACK;
-    timeUntil = ((currentDelta > 0) ? (int)((HARDCRACK_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = HARDCRACK;
-  } else if (currentTemp < CARAMEL_MIN) {
-    currentStage = HARDCRACK;
-    timeUntil = ((currentDelta > 0) ? (int)((CARAMEL_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = CARAMEL;
-  } else if (currentTemp < BURNTSUGAR_MIN) {
-    currentStage = CARAMEL;
-    timeUntil = ((currentDelta > 0) ? (int)((BURNTSUGAR_MIN - currentTemp) / currentDelta) : 0);
-    nextStage = BURNTSUGAR;
-  } else {
-    currentStage = BURNTSUGAR;
-    timeUntil = 0;
-    nextStage = "";
+void printStage(int currentTemp, int currentDelta, byte pos, int *stagesTemp, const descriptionType *stagesNames) {
+  byte stage = 0;
+  while (currentTemp > stagesTemp[stage] && stage < sizeof(stagesTemp)) {
+    stage++;
   }
-
+  unsigned int timeUntil = ((currentDelta > 0 && stage < sizeof(stagesTemp)) ? (int)((stagesTemp[stage] - currentTemp) / currentDelta) : 0);
+  descriptionType currentStage;
+  descriptionType nextStage;
+  memcpy_P (&currentStage, &stagesNames [stage], sizeof currentStage);
+  memcpy_P (&nextStage, &stagesNames [stage+1], sizeof currentStage);
   char timeToNextStage[17];
   snprintf(timeToNextStage, 17, "%d s avant", timeUntil);
 
   u8g2.setFont(SMALL_FONT);
   u8g2.setCursor(0, pos + SMLINE_HEIGHT);
-  u8g2.print(currentStage);
+  u8g2.print(currentStage.description);
   if (timeUntil > 0) {
     u8g2.setCursor(0, pos + 2 * SMLINE_HEIGHT);
     u8g2.print(timeToNextStage);
@@ -123,62 +105,93 @@ void candyMaking(int currentTemp, int currentDelta, byte pos) {
   u8g2.setCursor(0, pos + 3 * SMLINE_HEIGHT);
   u8g2.print(F("Prochain stade"));
   u8g2.setCursor(0, pos + 4 * SMLINE_HEIGHT);
-  u8g2.print(nextStage);
+  u8g2.print(nextStage.description);
+}
 
+void candyMaking(int currentTemp, int currentDelta, byte pos) {
+  const static descriptionType PROGMEM stagesNames[11]  = {{"Confiserie"},
+                                                           {"Petit filet/Soft thread"},
+                                                           {"Grand filet/Thread"},
+                                                           {"Petit boule/Soft ball"},
+                                                           {"Grand boule/Firm ball"},
+                                                           {"Boule/Hard ball"},
+                                                           {"Petit casse/Soft crack"},
+                                                           {"Grand casse/Hard crack"},
+                                                           {"Caramel"},
+                                                           {"Sucre brule/Burnt sugar"},
+                                                           {""}
+                                                          };
+  int stagesTemp[9] =  {10500, 10700, 11200, 11800.12500, 13500, 14500, 15000, 17100};
+  printStage(currentTemp, currentDelta, pos, stagesTemp, stagesNames);
 
 }
-void meatCooking(int currentTemp, int currentDelta, byte pos) {
-  u8g2.setFont(SMALL_FONT);
-  u8g2.setCursor(0, pos + SMLINE_HEIGHT);
-  u8g2.print("Mode viande");
+void beefCooking(int currentTemp, int currentDelta, byte pos) {
+  const static descriptionType PROGMEM stagesNames[5]  = {{"Boeuf/Agneau"},
+                                                          {"Saignant"},
+                                                          {"A point"},
+                                                          {"Bien cuit"},
+                                                          {""}
+                                                         };
+  int stagesTemp[9] =  {6000, 7000, 7500};
+  printStage(currentTemp, currentDelta, pos, stagesTemp, stagesNames);
+
+}
+void porcCooking(int currentTemp, int currentDelta, byte pos) {
+  const static descriptionType PROGMEM stagesNames[4]  = {{"Porc"},
+                                                          {"Fume"},
+                                                          {"Frais"},
+                                                          {""}
+                                                         };
+  int stagesTemp[9] =  {6000, 7500};
+  printStage(currentTemp, currentDelta, pos, stagesTemp, stagesNames);
+
+}
+void poultryCooking(int currentTemp, int currentDelta, byte pos) {
+  const static descriptionType PROGMEM stagesNames[5]  = {{"Volaille"},
+                                                          {"Poulet min"},
+                                                          {"Poulet max"},
+                                                          {"Canard"},
+                                                          {""}
+                                                         };
+  int stagesTemp[9] =  {8000, 8500, 9000};
+  printStage(currentTemp, currentDelta, pos, stagesTemp, stagesNames);
+
 }
 void alarmMode(int currentTemp, int currentDelta, byte pos) {
   int timeUntil = 0;
-  timeUntil = ((currentDelta > 0 && (alarmTemp * 100) > currentTemp) ? (int)((alarmTemp * 100 - currentTemp) / currentDelta) : 0);
+  timeUntil = ((currentDelta > 0 && (alarmTemp * 100) > currentTemp)||(currentDelta < 0 && (alarmTemp * 100) < currentTemp) ? (int)((alarmTemp * 100 - currentTemp) / currentDelta) : 0);
   char timeToAlarm[17];
   snprintf(timeToAlarm, 17, "%d s avant", timeUntil);
   u8g2.setFont(SMALL_FONT);
   u8g2.setCursor(0, pos + SMLINE_HEIGHT);
   u8g2.print(timeToAlarm);
   u8g2.setCursor(0, pos + 2 * SMLINE_HEIGHT);
-  u8g2.print("alarme a:");
+  u8g2.print(F("alarme a:"));
   u8g2.print(alarmTemp);
-  u8g2.print("C / ");
+  u8g2.print(F("C / "));
   u8g2.print((int)(alarmTemp * 1.8 + 32));
-  u8g2.print("F");
+  u8g2.print(F("F"));
 
 }
 
 void loop(void) {
-  if (checkForFaults()) {
-    u8g2.firstPage();
-    do {
-      u8g2.setFont(DEF_FONT);
-      u8g2.setCursor(0, LINE_HEIGHT);
-      u8g2.print("Erreur");
-    } while ( u8g2.nextPage() );
-    delay(2000);
-  }
+  checkForFaults();
   //  while (Serial.available() > 0) {
   //    RNOMINAL = Serial.parseFloat();
   //      Serial.print("changed RNOMINAL to ");
   //      Serial.println(RNOMINAL);
   //  }
 
-
-  int currentTemp = (int)(max3.temperature(RNOMINAL, RREF) * 100);
-  char temperature[9];
-  snprintf(temperature, 9, "%.2fC ", ((float)currentTemp) / 100);
-  char temperatureF[9];
-  snprintf(temperatureF, 9, "%.2fF ", (((float)currentTemp) / 100) * 1.8 + 32);
-
-  int currentDelta = currentTemp - prevTemp;
-
   char delta[8];
   char deltaF[8];
+    char temperature[9];
+  char temperatureF[9];
+  int currentTemp = (int)(max3.temperature(RNOMINAL, RREF) * 100);
+    int currentDelta = currentTemp - prevTemp;
 
+  snprintf(temperature, 9, "%.2fC ", ((float)currentTemp) / 100);
+  snprintf(temperatureF, 9, "%.2fF ", (((float)currentTemp) / 100) * 1.8 + 32);
   if (currentDelta < 0) {
-
     snprintf( delta, 8, "▽%.2f", ((float)currentDelta * -1) / 100);
     snprintf( deltaF, 8, "▽%.2f", (((float)currentDelta) * -1 / 100) * 1.8);
   } else {
@@ -201,7 +214,9 @@ void loop(void) {
     u8g2.print(deltaF);
     u8g2.drawHLine(0, 2 * LINE_HEIGHT + 1, 128);
     if (tmode == CANDY_MODE) candyMaking(currentTemp, currentDelta, 2 * LINE_HEIGHT + 1);
-    if (tmode == MEAT_MODE) meatCooking(currentTemp, currentDelta, 2 * LINE_HEIGHT + 1);
+    if (tmode == BEEF_MODE) beefCooking(currentTemp, currentDelta, 2 * LINE_HEIGHT + 1);
+    if (tmode == PORC_MODE) porcCooking(currentTemp, currentDelta, 2 * LINE_HEIGHT + 1);
+    if (tmode == POULTRY_MODE) poultryCooking(currentTemp, currentDelta, 2 * LINE_HEIGHT + 1);
     if (tmode == ALARM_MODE) alarmMode(currentTemp, currentDelta, 2 * LINE_HEIGHT + 1);
   } while ( u8g2.nextPage() );
 
@@ -209,54 +224,50 @@ void loop(void) {
   int endtime = starttime;
   while ((endtime - starttime) <= 1000)
   {
+    yield(); //FOR ESP8266 ONLY
     int event = u8g2.getMenuEvent();
     if (event != 0) {
       if (event == U8X8_MSG_GPIO_MENU_SELECT) {
-        if (tmode < 4) tmode++;
+        if (tmode < ALARM_MODE) tmode++;
         else tmode = 1;
         break;
       }
 
     }
     int loopcount = 1;
-    while ((digitalRead(14) == LOW || digitalRead(16) == LOW) && tmode == 4) {
+    while ((digitalRead(14) == LOW || digitalRead(16) == LOW) && tmode == ALARM_MODE) {
       int substart = millis();
       int subend = substart;
       int delaylength = 500;
       while ((subend - substart) < 3000) {
-        //yield(); //FOR ESP8266 ONLY
+        yield(); //FOR ESP8266 ONLY
         if (digitalRead(14) == HIGH && digitalRead(16) == HIGH) loopcount = 1;
         u8g2.firstPage();
         do {
 
           u8g2.setFont(DEF_FONT);
           u8g2.setCursor(0, LINE_HEIGHT);
-          u8g2.print("Alarme pour");
+          u8g2.print(F("Alarme pour"));
           u8g2.setCursor(0, 2 * LINE_HEIGHT);
           u8g2.print(alarmTemp);
-          u8g2.print("C / ");
+          u8g2.print(F("C / "));
           u8g2.print((int)(alarmTemp * 1.8 + 32));
-          u8g2.print("F");
+          u8g2.print(F("F"));
           u8g2.drawHLine(0, 2 * LINE_HEIGHT + 1, 128);
 
         } while ( u8g2.nextPage() );
 
-        if (digitalRead(14) == LOW) {
-          if (alarmTemp > 0) {
-            alarmTemp -= 1;
-            delay((int)(delaylength / (2 * loopcount)));
-          }
-
-        } else if (digitalRead(16) == LOW) {
-          if (alarmTemp < 400) {
-            alarmTemp += 1;
-            delay((int)(delaylength / (2 * loopcount)));
-          }
+        if (digitalRead(14) == LOW && alarmTemp > 0) {
+          alarmTemp -= 1;
+          delay((int)(delaylength / (2 * loopcount)));
+        } else if (digitalRead(16) == LOW && alarmTemp < 400) {
+          alarmTemp += 1;
+          delay((int)(delaylength / (2 * loopcount)));
         }
-        if (loopcount < 10) loopcount++;
+
+        if (loopcount < 20) loopcount++;
         if (digitalRead(14) == LOW || digitalRead(16) == LOW) substart = millis();
         subend = millis();
-
       }
     }
     endtime = millis();
